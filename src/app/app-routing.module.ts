f33dfc86b9782616662b83613208612dfbb270ff
Ranyere Ramires <ranyereramires@gmail.com>
2024-08-08 19:55:06 -0300

    criação de módulos de rota e declaração de módulos de rota no app routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }
+  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'eventos', loadChildren: () => import('./pages/eventos/eventos.module').then(m => m.EventosModule) },
+  { path: 'solicitacoes', loadChildren: () => import('./pages/solicitacoes/solicitacoes.module').then(m => m.SolicitacoesModule) },
+  { path: 'vagas', loadChildren: () => import('./pages/vagas/vagas.module').then(m => m.VagasModule) }
 ];
 
 @NgModule({
---
+++ src/app/pages/vagas/vagas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VagasRoutingModule } from './vagas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VagasRoutingModule
+  ]
+})
+export class VagasModule { }
